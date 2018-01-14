cca2aca06fcd4e876dab1015adff9a04863cae0c
Nathan Fennel <nathanfennel@gmail.com>
2018-01-14 18:49:46 -0500

    Added more constants

--- BlueRecipes/Constants/Constants.swift
+++ BlueRecipes/Constants/Constants.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 class Constants {
+    /// ","
+    static let apiConcatenationSeparator = ","
     /// "http://food2fork.com/api/get"
     static let baseRecipeRequestURLString = "http://food2fork.com/api/get"
     /// "http://food2fork.com/api/get" as! URL
@@ -17,4 +19,8 @@ class Constants {
     static let baseSearchURLString = "http://food2fork.com/api/search"
     /// "http://food2fork.com/api/search" as! URL
     static let baseSearchURL = URL(string: baseSearchURLString)!
+    /// 30
+    static let maxRecipeRequestCount: Int = 30
+    /// "Food2Fork"
+    static let serviceName = "Food2Fork"
 }
