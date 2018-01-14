5ad4bc3faba3930a9cb45dd8e36ca852ce699eba
Nathan Fennel <nathanfennel@gmail.com>
2018-01-14 17:31:13 -0500

    Added Pods and remaining API Keys

---
+++ BlueRecipes/APIKeys.swift
@@ -0,0 +1,46 @@
+//
+//  APIKeys.swift
+//  BlueRecipes
+//
+//  Created by Nathan Fennel on 1/14/18.
+//  Copyright © 2018 Nathan Fennel. All rights reserved.
+//
+
+import Foundation
+
+class APIKeys {
+    /// "count"
+    static let count = "count"
+    /// "f2f_url"
+    static let food2ForkURL = "f2f_url"
+    /// "image_url"
+    static let imageURL = "image_url"
+    /// "ingredients"
+    static let ingredients = "ingredients"
+    /// "key"
+    static let key = "key"
+    /// "page"
+    static let page = "page"
+    /// "publisher"
+    static let publisher = "publisher"
+    /// "publisher_url"
+    static let publisherURL = "publisher_url"
+    /// "q"
+    static let query = "q"
+    /// "r"
+    static let rating = "r"
+    /// "rId"
+    static let recipeID = "rId"
+    /// "recipes"
+    static let recipes = "recipes"
+    /// "social_rank"
+    static let socialRank = "social_rank"
+    /// "sort"
+    static let sort = "sort"
+    /// "source_url"
+    static let sourceURL = "source_url"
+    /// "title"
+    static let title = "title"
+    /// "t"
+    static let trendScore = "t"
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BlueRecipes' do
+    use_frameworks!
+    platform :ios, '10.0'
+    use_frameworks!
+    `$(inherited)`
+    pod ‘Alamofire’
+    pod 'Pastel'
+    pod 'PureLayout'
+    pod 'Unbox'
+    inherit! :search_paths
+end
