fa58314e034e974e621b5895f8a2dca9e0219dc8
Nathan Fennel <nathanfennel@gmail.com>
2018-01-14 17:32:26 -0500

    Added RecipeModel

---
+++ BlueRecipes/Constants/PrivateConstants.swift
@@ -0,0 +1,14 @@
+//
+//  PrivateConstants.swift
+//  BlueRecipes
+//
+//  Created by Nathan Fennel on 1/14/18.
+//  Copyright © 2018 Nathan Fennel. All rights reserved.
+//
+
+import Foundation
+
+class PrivateConstants {
+    /// 88096747e479919f373ac421939b8477
+    static let apiKey = "88096747e479919f373ac421939b8477"
+}
