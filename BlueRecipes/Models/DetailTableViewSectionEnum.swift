9c1ec76499fdf58cf783f5b5680b9e007c4b1441
Nathan Fennel <nathanfennel@gmail.com>
2018-01-15 17:29:10 -0500

    Added enum for handling layout logic of detail table view

---
+++ BlueRecipes/Models/DetailTableViewSectionEnum.swift
@@ -0,0 +1,9 @@
+//
+//  DetailTableViewSectionEnum.swift
+//  BlueRecipes
+//
+//  Created by Nathan Fennel on 1/15/18.
+//  Copyright © 2018 Nathan Fennel. All rights reserved.
+//
+
+import Foundation
